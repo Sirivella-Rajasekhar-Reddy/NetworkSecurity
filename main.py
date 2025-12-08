5174c54552dd91f26d3ee01596fe1672df0dc94e
Sirivella-Rajasekhar-Reddy <105851813+Sirivella-Rajasekhar-Reddy@users.noreply.github.com>
2025-12-09 00:02:01 +0530

    Model Training and MLFlow

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 import sys
 
 if __name__=='__main__':
@@ -33,6 +36,14 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
+
+
+        logging.info("Model Training Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        print(model_trainer_artifact)
+        logging.info("Model Training artifact created")
     
     except Exception as e:
            raise NetworkSecurityException(e,sys)
